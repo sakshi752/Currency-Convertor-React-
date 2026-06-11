f257ad2b25fb7026db315cc58a7fd4dc9c09c3aa
Sakshi <saks0705@gmail.com>
2026-06-11 19:05:25 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function App() {
 
   const options = Object.keys(currencyInfo);
 
-  useEffect(() => {
+useEffect(() => {
   if (currencyInfo[to]) {
-    convert();
+    setConvertedAmount(amount * currencyInfo[to]);
   }
-}, [from, to]);
+}, [amount, from, to, currencyInfo]);
 
   const swap = () => {
     setFrom(to)
@@ -27,6 +27,7 @@ function App() {
   }
 
   const convert = () => {
+    console.log("to ",currencyInfo[to])
     setConvertedAmount(amount * currencyInfo[to])
   }
   
@@ -35,20 +36,22 @@ function App() {
     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-gray-700'>
       <div className='w-full max-w-lg mx-auto border border-gray-400 p-5 rounded bg-white/30 backdrop-blur-2xl'>
         <h2 className='flex items-center justify-center mb-4 text-xl font-bold text-white tracking-wider'>Currency Convertor</h2>
-        <form action="" onSubmit={(e) => {
-          e.preventDefault();
-          convert();
-        }}>
+        <form action=""
+        //  onSubmit={(e) => {
+        //   e.preventDefault();
+        //   convert();
+        // }}
+        >
           <div className='mb-1'>
             <InputBox label={"From"} amount={amount}  onAmountChange={(amt) => setAmount(amt)} onCurrencyChange={(curr) => setFrom(curr)} currencyOptions={options} selectedCurrency={from} />
           </div>
           <div className="relative w-full h-0.5">
-            <button className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 px-2 py-0.5 bg-blue-600 text-white rounded-md cursor-pointer' onClick={swap}>Swap</button>
+            <button  type="button" className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 px-2 py-0.5 bg-blue-600 text-white rounded-md cursor-pointer' onClick={swap}>Swap</button>
           </div>
           <div className='my-1'>
             <InputBox label={"From"} amount={convertedAmount} convertedAmount={convertedAmount} amountDisable onCurrencyChange={(curr) => setTo(curr)} currencyOptions={options} selectedCurrency={to} />
           </div>
-          <button type='submit' className='bg-blue-700 w-full rounded-md py-2 text-white text-lg font-bold cursor-pointer'>Convert {from} to {to}</button>
+          {/* <button type='submit' className='bg-blue-700 w-full rounded-md py-2 text-white text-lg font-bold cursor-pointer'>Convert {from} to {to}</button> */}
         </form>
 
       </div>
